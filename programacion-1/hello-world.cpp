f0ff7e4ab33722a484099912d6ac0eccd61248b6
gabriel.camps <ext_nalbberra@falabella.cl>
2021-06-07 11:13:29 -0500

    [ADD] hello-world

---
+++ programacion-1/hello-world.cpp
@@ -0,0 +1,18 @@
+//la librería iostream permite mostrar datos en la consola y capturar datos desde el teclado
+#include <iostream>
+
+// es necesario incluir esta línea para utilizar cin y cout
+using namespace std;
+
+// la funcion main( main significa 'principal' en inglés) es por donde se empieza a ejecutar
+// un programa de C++. Solo puede exisir una función main por proyecto
+int main()
+{
+  
+    // imprimimos en pantalla 
+    cout << "Hello, World!"  << endl;
+
+    // con el system("pause") le decimos a la consola que no se cierre una vez   terminado el programa
+    system("pause");
+    return 0;
+}
